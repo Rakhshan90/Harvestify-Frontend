4175859bb0bcdd9cf521fdda94947471aea4740f
Rakhshan <rakhshancoder@gmail.com>
2024-03-21 02:58:44 +0530

    login and signup components are implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 // import './App.css'
 
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navigation/Navbar"
+import Login from "./Components/User/Login"
+import Register from "./Components/User/Register"
+import Home from "./Components/Home"
 
 function App() {
 
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
